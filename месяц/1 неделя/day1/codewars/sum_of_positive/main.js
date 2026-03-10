9882952c29399b248d50ddcccbd9e460ff45c079
artempoo <77047966+artempoo@users.noreply.github.com>
2026-03-10 19:16:27 +0300

    обнуляю предыдущий опыт

--- месяц/1 неделя/day1/codewars/sum_of_positive/main.js
+++
@@ -1,8 +0,0 @@
-function positiveSum(arr) {
-  let acc = 0;
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) acc += arr[i];
-  }
-
-  return acc;
-}
--- месяц/1 неделя/day1/materials.md
+++
@@ -1,1 +0,0 @@
-https://mitpress.mit.edu/algorithms
